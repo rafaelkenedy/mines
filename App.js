73405a19b64199b5540c3afa358f135cfcf940f5
rafaelkenedy <k.rafaelalves@gmail.com>
2021-12-18 01:36:14 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import params from './src/params'
+import Field from './src/components/Field'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!!</Text>
+        <Text style={styles.welcome}>
+          Tamanho da grade: {params.getRowsAmount()}
+        </Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+})
